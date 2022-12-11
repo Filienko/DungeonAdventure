1ad8d5ea507ead026a56287ac79ccf47e57997b8
Daniil <94884910+Filienko@users.noreply.github.com>
2022-12-10 19:29:23 -0800

    Modified to account for destruction of pillars

--- core/src/MVC/Model/DungeonItems/Items/Pillar.java
+++ core/src/MVC/Model/DungeonItems/Items/Pillar.java
@@ -2,8 +2,6 @@ package MVC.Model.DungeonItems.Items;
 
 import MVC.Model.DungeonAdventure.DungeonCharacters.EntityFactory;
 import MVC.Model.DungeonAdventure.DungeonCharacters.Hero;
-import MVC.Model.Physics.Physics;
-import MVC.Model.Physics.Vec2;
 
 public class Pillar extends Item
 {
