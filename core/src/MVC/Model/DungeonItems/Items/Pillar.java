180259ea88139b494fb2dcc302763de0379aa992
abi <abigtrz2021@gmail.com>
2022-12-14 14:58:13 -0800

    fixed pillar bug where it broke before all monsters in the room were killed + updated PillarTest

--- core/src/MVC/Model/DungeonItems/Items/Pillar.java
+++ core/src/MVC/Model/DungeonItems/Items/Pillar.java
@@ -8,28 +8,37 @@ public class Pillar extends Item
 {
     private String myName;
     private boolean myBroken;
+    private int myMonsterCounter;
 
     public Pillar(final EntityFactory theEntityFactory)
     {
         super("pillar", theEntityFactory);
         setName("pillar");
         setMySize(new Vec2(64, 64));
+        setMonsterCounter(0);
         myBroken = false;
     }
 
-    public Pillar(final String theName, final EntityFactory theEntityFactory)
+    public Pillar(final String theName, final int theMonsterCounter, final EntityFactory theEntityFactory)
     {
         super(theName, theEntityFactory);
         setName(theName);
         setMySize(new Vec2(64, 64));
+        setMonsterCounter(theMonsterCounter);
         myBroken = false;
     }
 
     public void breakPillar()
     {
-        setMyAnimation(getMyEntityFactory().getAssets().getAnimation("brokenPillar"));
-        getMyEntityFactory().getHero().incrementPillars();
-        myBroken = true;
+        if (myMonsterCounter == 0)
+        {
+            if (getMyEntityFactory().getAssets() != null) //added for testing, should i delete?
+            {
+                setMyAnimation(getMyEntityFactory().getAssets().getAnimation("brokenPillar"));
+            }
+            getMyEntityFactory().getHero().incrementPillars();
+            myBroken = true;
+        }
     }
 
     public String getName()
@@ -38,6 +47,31 @@ public class Pillar extends Item
     }
 
     public boolean isBroken() { return myBroken; }
+
+    public int getMonsterCounter()
+    {
+        return myMonsterCounter;
+    }
+
+    public void setMonsterCounter(final int theMonsterCounter)
+    {
+        if (theMonsterCounter > 0)
+        {
+            myMonsterCounter = theMonsterCounter;
+        }
+        else
+        {
+            decrementMonsterCounter();
+        }
+    }
+
+    public void decrementMonsterCounter()
+    {
+        if (myMonsterCounter > 0)
+        {
+            myMonsterCounter--;
+        }
+    }
 
     private void setName(final String theName)
     {
